dc421bb07f6e07432a349347ec5e262e118704e3
HassanKhan123 <hk2602809@gmail.com>
2020-10-17 06:48:58 +0500

    building single file components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+    {{ user.userName }} - {{ fullName }} <strong>Followers: </strong>{{ followers }}
+    <button @click="followUser">Follow</button>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    data() {
+        return {
+            followers: 0,
+            user: {
+                id: 1,
+                userName: "HassanKhan123",
+                firstName: "Hassan",
+                lastName: "Khan",
+                email: "hassan@gmail.com",
+                isAdmin: true,
+            },
+        };
+    },
+    watch: {
+        followers(newFollowers, oldFollowers) {
+            if (oldFollowers < newFollowers) {
+                console.log(`${this.user.userName} has gained followers`)
+            }
+        }
+    },
+    computed: {
+        fullName() {
+            return `${this.user.firstName} ${this.user.lastName}`;
+        },
+    },
+    methods: {
+        followUser() {
+            this.followers++;
+        },
+    },
+    mounted() {
+        this.followUser()
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
 }
 </style>
